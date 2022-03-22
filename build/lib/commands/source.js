628063315127066981a158dd6b2f9fd1a9f489e0
饭桶狼 ra～ <96805917+fantonglang@users.noreply.github.com>
2022-03-22 10:30:29 +0800

    add source command

---
+++ build/lib/commands/source.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+require("source-map-support/register");
+
+commands = {};
+
+commands.source = async function source(opts = {}) {
+  return `<application></application>`;
+};
+
+var _default = commands;
+exports.default = _default;require('source-map-support').install();
+
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImxpYi9jb21tYW5kcy9zb3VyY2UuanMiXSwibmFtZXMiOlsiY29tbWFuZHMiLCJzb3VyY2UiLCJvcHRzIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQUEsUUFBUSxHQUFHLEVBQVg7O0FBRUFBLFFBQVEsQ0FBQ0MsTUFBVCxHQUFrQixlQUFlQSxNQUFmLENBQXVCQyxJQUFJLEdBQUcsRUFBOUIsRUFBa0M7QUFDbEQsU0FBUSw2QkFBUjtBQUNELENBRkQ7O2VBSWVGLFEiLCJzb3VyY2VzQ29udGVudCI6WyJjb21tYW5kcyA9IHt9O1xuXG5jb21tYW5kcy5zb3VyY2UgPSBhc3luYyBmdW5jdGlvbiBzb3VyY2UgKG9wdHMgPSB7fSkge1xuICByZXR1cm4gYDxhcHBsaWNhdGlvbj48L2FwcGxpY2F0aW9uPmA7XG59O1xuXG5leHBvcnQgZGVmYXVsdCBjb21tYW5kcztcbiJdLCJmaWxlIjoibGliL2NvbW1hbmRzL3NvdXJjZS5qcyIsInNvdXJjZVJvb3QiOiIuLi8uLi8uLiJ9
