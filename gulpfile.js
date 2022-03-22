e54b9dd2407278f30e75fbb6a68613a1ae367a6c
ubuntu <ubuntu@fantonglang.com>
2022-03-22 14:00:04 +0800

    move the native part to other porj

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let gulp = require('gulp');
 let boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
-  build: 'appium-mac2-driver',
+  build: 'appium-linux-driver',
   files: ['index.js', 'lib/**/*.js', 'test/**/*.js', '!gulpfile.js'],
   coverage: {
     files: ['./test/unit/**/*-specs.js', '!./test/functional/**'],
