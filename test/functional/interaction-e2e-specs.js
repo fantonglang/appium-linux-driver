2f25719d79bbbc567aa478eb47d0b28fac1d6bcd
ubuntu <ubuntu@fantonglang.com>
2022-03-27 13:18:20 +0800

    basic e2e tests

--- test/functional/interaction-e2e-specs.js
+++ test/functional/interaction-e2e-specs.js
@@ -45,60 +45,26 @@ describe('AtSpi2Driver - elements interaction', function () {
     }
   });
 
-  it('should set a text to a text view', async function () {
+  it('should set a text to a text input control', async function () {
+    // 1. steps to set text
     const el = await driver.findElement('name', 'Find');
     await driver.executeScript('linux: click', [el.ELEMENT]);
-    const txtEl = await driver.findElement('xpath', '(//text[@name="Search"])[1]');
+    let txtEl = await driver.findElement('xpath', '(//text[@name="Search"])[1]');
     await driver.executeScript('linux: click', [txtEl.ELEMENT]);
     await driver.elementSendKeys(txtEl.ELEMENT, ['hello world']);
-    await wait4sec(1);
+    await wait4sec(0.5);
+    // 2. refresh the element state
+    txtEl = await driver.findElement('xpath', '(//text[@name="Search"])[1]');
     await driver.getElementText(txtEl.ELEMENT).should.eventually.eql('hello world');
   });
 
-  // it('should click a button by absolute coordinate', async function () {
-  //   const el = _.first(await driver.findElements('-ios predicate string', 'elementType == 12 AND label == "bold"'));
-  //   const {x, y, width, height} = await driver.getElementAttribute(el, 'frame');
-  //   await driver.executeScript('macos: click', {
-  //     x: x + width / 2,
-  //     y: y + height / 2,
-  //   });
-  //   const els = await driver.findElements('-ios predicate string', 'value == "Bold" AND label == "type face"');
-  //   els.length.should.eql(1);
-  // });
-
-  // it('should clear a text view', async function () {
-  //   const el = await driver.findElement('class name', 'XCUIElementTypeTextView');
-  //   await driver.elementSendKeys(el, 'hello world');
-  //   await driver.getElementText(el).should.eventually.eql('hello world');
-  //   await driver.elementClear(el);
-  //   await driver.getElementText(el).should.eventually.eql('');
-  // });
-
-  // it('should send keys with modifiers into a text view', async function () {
-  //   const el = await driver.findElement('class name', 'XCUIElementTypeTextView');
-  //   await driver.elementClick(el);
-  //   const flagsShift = 1 << 1;
-  //   await driver.executeScript('macos: keys', {
-  //     keys: [{
-  //       key: 'h',
-  //       modifierFlags: flagsShift,
-  //     }, {
-  //       key: 'i',
-  //       modifierFlags: flagsShift,
-  //     }]
-  //   });
-  //   await driver.getElementText(el).should.eventually.eql('HI');
-  // });
-
-  // it('should open context menu if left click with Ctrl depressed', async function () {
-  //   const el = await driver.findElement('class name', 'XCUIElementTypeTextView');
-  //   const flagsCtrl = 1 << 2;
-  //   await driver.executeScript('macos: click', {
-  //     elementId: el,
-  //     keyModifierFlags: flagsCtrl,
-  //   });
-  //   const els = await driver.findElements('-ios predicate string', `title == 'Import Image'`);
-  //   els.length.should.be.above(1);
-  // });
+  it('should click a button', async function () {
+    // 1. click on the Find button
+    const el = await driver.findElement('name', 'Find');
+    await driver.executeScript('linux: click', [el.ELEMENT]);
+    // 2. text input should appear after then
+    const txtEls = await driver.findElements('xpath', '(//text[@name="Search"])[1]');
+    txtEls.length.should.eql(1);
+  });
 
 });
